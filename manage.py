e5627889426d404f11a76904d6a49cef4ac2a7d0
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-11-06 18:10:53 +0300

    Update run file - models and migrations

--- manage.py
+++ manage.py
@@ -1,7 +1,8 @@
 import unittest
 from app import create_app
 from flask_script import Manager, Server
-
+from flask_migrate import Migrate, MigateCommand
+from app.models import County, Constituency, Party, Mca, Governor, DeputyGovernor, Senator, WomanRep, User
 
 app = create_app('development')
 
@@ -9,7 +10,12 @@ app = create_app('development')
 
 
 manager = Manager(app)
+
 manager.add_command('server', Server)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+
+
 @manager.command
 def test():
     '''
@@ -18,10 +24,11 @@ def test():
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
 
+
 @manager.shell
 def make_shell_context():
+    return dict(app=app, db=db, County=County, Constituency=Constituency, Party=Party, Mca=Mca, Governor=Governor, Senator=Senator, DeputyGovernor=DeputyGovernor, WomanRep=WomanRep, User=User)
 
-    return dict(app=app)
 
 if __name__ == '__main__':
     manager.run()
