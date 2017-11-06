df7913d8ab42d28073a993cd4f4a0b4d931b2c66
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-11-06 18:14:33 +0300

    Add extra configurations

--- config.py
+++ config.py
@@ -1,25 +1,29 @@
 import os
 
+
 class Config:
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
-
-    @staticmethod
-    def init_app(app):
-        pass
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class ProdConfig(Config):
-
+    # SQLALCHEMY_DATABASE_URI = os.environ.get("HEROKU_POSTGRESQL_YELLOW_URL")
     pass
 
 
 class DevConfig(Config):
-
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://monster:Hummingbirdcomp#@localhost/county_govt'
     Debug = True
+
+
+class TestConfig(Config):
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://monster:Hummingbirdcomp#@localhost/county_govt_test'
+    pass
 
 
 config_options = {
     'development': DevConfig,
-    'production': ProdConfig
+    'production': ProdConfig,
+    'test': TestConfig
 }
