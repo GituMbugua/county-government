ac6fdca0b0c943a022abfc8ea51d7e86a4913b8c
Virginia Ndung'u <ndungu.wairimu22@gmail.com>
2017-11-08 16:45:27 +0300

    Update models - authentication

--- app/models.py
+++ app/models.py
@@ -11,7 +11,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(60), index=True, unique=True)
     username = db.Column(db.String(60), index=True, unique=True)
-    fullname = db.Column(db.String(60), index=True)
+    fullname = db.Column(db.String(256), index=True)
     user_pwd = db.Column(db.String(128))
     is_admin = db.Column(db.Boolean, default=False)
     # relationships
@@ -26,6 +26,33 @@ class User(db.Model, UserMixin):
     womenreps = db.relationship('WomanRep', backref='user', lazy='dynamic')
     mcas = db.relationship('MCA', backref='user', lazy='dynamic')
 
+    @property
+    def password(self):
+        '''
+        prevent password from being accessed
+        '''
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        '''
+        Set password to a hashed password
+        '''
+        self.user_pwd = generate_password_hash(password)
+
+    def verify_password(self, password):
+        '''
+        check if hashed pwd matches actual pwd
+        '''
+        return check_password_hash(self.user_pwd, password)
+
+    def save_user(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return '<Reader: {}>'.format(self.username)
+
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(int(user_id))
@@ -35,16 +62,19 @@ class County(db.Model):
     __tablename__ = "counties"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(256))
-    # county_code = db.Column(db.String(60))
+    # foreign keys
+    user_id = db.Column(db.ForeignKey('users.id'))
     # relationships
     constituencies = db.relationship(
         'Constituency', backref='county', lazy='dynamic')
     governors = db.relationship('Governor', backref='county', lazy='dynamic')
     deputygovernors = db.relationship(
-        'DeputyGovernor', backref='county', lazy='dynamic')
-    senators = db.relationship('Senator', backref='county', lazy='dynamic')
-    womenreps = db.relationship('WomanRep', backref='county', lazy='dynamic')
-    mcas = db.relationship('MCA', backref='county', lazy='dynamic')
+        'DeputyGovernor', backref='dpgovernor', lazy='dynamic')
+    senators = db.relationship(
+        'Senator', backref='countysenator', lazy='dynamic')
+    womenreps = db.relationship(
+        'WomanRep', backref='countywomen', lazy='dynamic')
+    mcas = db.relationship('MCA', backref='countymcas', lazy='dynamic')
 
 
 class Constituency(db.Model):
@@ -53,6 +83,7 @@ class Constituency(db.Model):
     constituency_code = db.Column(db.Integer)
     name = db.Column(db.String(256))
     # foreign keys
+    # user_id = db.Column(db.ForeignKey('users.id'))
     county_code = db.Column(db.Integer, db.ForeignKey('counties.id'))
     # relationships
     mca = db.relationship('MCA', backref='constituency', lazy='dynamic')
@@ -64,6 +95,8 @@ class Party(db.Model):
     party_code = db.Column(db.String(60))
     name = db.Column(db.String(256))
     abrv = db.Column(db.String(256))
+    # foreign keys
+    user_id = db.Column(db.ForeignKey('users.id'))
     # relationships
     governors = db.relationship(
         'Governor', backref='party', lazy='dynamic')
@@ -81,6 +114,7 @@ class Governor(db.Model):
     surname = db.Column(db.String(256))
     othernames = db.Column(db.String(256))
     # Foreign keys
+    user_id = db.Column(db.ForeignKey('users.id'))
     county_code = db.Column(db.Integer, db.ForeignKey('counties.id'))
     party_code = db.Column(db.Integer, db.ForeignKey('parties.id'))
     # relationships
@@ -95,6 +129,7 @@ class DeputyGovernor(db.Model):
     surname = db.Column(db.String(256))
     othernames = db.Column(db.String(256))
     # Foreign keys
+    user_id = db.Column(db.ForeignKey('users.id'))
     county_code = db.Column(db.Integer, db.ForeignKey('counties.id'))
     party_code = db.Column(db.Integer, db.ForeignKey('parties.id'))
     governor_id = db.Column(db.Integer, db.ForeignKey('governors.id'))
@@ -106,6 +141,7 @@ class Senator(db.Model):
     surname = db.Column(db.String(256))
     othernames = db.Column(db.String(256))
     # Foreign keys
+    user_id = db.Column(db.ForeignKey('users.id'))
     county_code = db.Column(db.Integer, db.ForeignKey('counties.id'))
     party_code = db.Column(db.Integer, db.ForeignKey('parties.id'))
 
@@ -116,6 +152,7 @@ class WomanRep(db.Model):
     surname = db.Column(db.String(256))
     othernames = db.Column(db.String(256))
     # Foreign keys
+    user_id = db.Column(db.ForeignKey('users.id'))
     county_code = db.Column(db.Integer, db.ForeignKey('counties.id'))
     party_code = db.Column(db.Integer, db.ForeignKey('parties.id'))
 
@@ -126,6 +163,7 @@ class MCA(db.Model):
     surname = db.Column(db.String(256))
     othernames = db.Column(db.String(256))
     # Foreign keys
+    user_id = db.Column(db.ForeignKey('users.id'))
     constituency_code = db.Column(
         db.Integer, db.ForeignKey('constituencies.id'))
     county_code = db.Column(db.Integer, db.ForeignKey('counties.id'))
