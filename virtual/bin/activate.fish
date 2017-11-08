3af5dd53e0d190af93806b2379c92c135aaddae1
toelapiut <toelapiut7@gmail.com>
2017-11-08 07:51:50 +0300

    added flask packages

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/monster/Documents/python/county-government/virtual"
+set -gx VIRTUAL_ENV "/home/toel/Documents/python/flask/county-government/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
